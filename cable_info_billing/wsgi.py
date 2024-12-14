0519c1fa56404eb8725333ad9e41bfa8c4eacfb1
lazyPy <janopatundog@gmail.com>
2024-12-14 13:13:39 +0800

    .

--- cable_info_billing/wsgi.py
+++ cable_info_billing/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cable_info_billing.settings')
 
 application = get_wsgi_application()
+
+app = application
